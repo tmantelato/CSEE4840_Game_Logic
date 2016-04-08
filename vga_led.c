4780a5a681519dce24439efc445020b49038e0cb
Henrique Pizzol Grando <hp2409@columbia.edu>
2016-04-07 21:42:06 -0400

    Driver files for vga

---
+++ vga_led.c
@@ -0,0 +1,156 @@
+/*
+ * Device driver for the VGA LED Emulator
+ *
+ * References:
+ * http://www.linuxforu.com/tag/linux-device-drivers/
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <asm/io.h>
+
+#include "vga_led.h"
+#include "socal/hps.h"
+#include "hps_0.h"
+
+/* Memory region visible through the lightweight HPS-FPGA bridge */
+#define HW_REGS_BASE ALT_LWFPGASLVS_OFST
+#define HW_REGS_SIZE 0x200000
+#define HW_REGS_MASK (HW_REGS_SIZE - 1)
+
+#define WRITE_BYTE(addr, val)  ( *(volatile unsigned int *)(addr) = (val))
+
+static void __iomem *registers; /* Base of mapped memory */
+static dev_t firstdev;
+static struct class *cl;
+static struct cdev c_dev;
+
+static void *screen_registers; /* Start of registers for LEDs */
+
+/* Current center state, since the hardware registers can't be read */
+static sprite_info sp_info;
+static screen background_info;
+
+static void write_screen(sprite_info sprite, screen background) {
+  	uint32_t spr, bgnd;
+  	spr = 0;
+  	bgnd = 0;
+  	spr = (((unsigned int)sprite.sprite_x) & 0x3ff) |
+  			 ((((unsigned int)sprite.sprite_y) & 0x3ff) << 10) | 
+  			 ((((unsigned int)sprite.shape) & 0xf) << 20) | 
+  			 ((((unsigned int)sprite.count) & 0xf) << 24) | 
+  			 ((((unsigned int)sprite.layer) & 0x7) << 28);
+ 	
+ 	bgnd = (((unsigned int)background.life_1) & 0xf) |
+ 	       ((((unsigned int)background.life_2) & 0xf) << 4) |
+  	       (((background.background_color) & 0xffffff) << 8);
+
+
+  	WRITE_BYTE(screen_registers + 0, spr);
+  	WRITE_BYTE(screen_registers + 4, bgnd);
+  	sp_info = sprite;
+  	background_info = background;
+}
+
+static int my_open(struct inode *i, struct file *f) {
+  return 0;
+}
+
+static int my_close(struct inode *i, struct file *f) {
+  return 0;
+}
+
+/* Handle ioctls(): write to the display registers or read our state */
+static long my_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+{
+  vga_screen_arg_t vla;
+
+  switch (cmd) {
+  case VGA_LED_WRITE_DIGIT:
+    if (copy_from_user(&vla, (vga_screen_arg_t *) arg, sizeof(vga_screen_arg_t)))
+      return -EACCES;
+
+  	
+    write_screen(vla.sprite, vla.background);
+    break;
+    
+  case VGA_LED_READ_DIGIT:
+    if (copy_from_user(&vla, (vga_screen_arg_t *) arg, sizeof(vga_screen_arg_t)))
+      return -EACCES;
+    vla.sprite = sp_info;
+    vla.background = background_info;
+    if (copy_to_user((vga_screen_arg_t *) arg, &vla, sizeof(vga_screen_arg_t)))
+      return -EACCES;
+    break;
+
+  default:
+    return -EINVAL;
+  }
+
+  return 0;
+}
+
+static struct file_operations my_fops = {
+  .owner = THIS_MODULE,
+  .open = my_open,
+  .release = my_close,
+  .unlocked_ioctl = my_ioctl
+};
+
+/* Initialize the driver: map the hardware registers, register the
+ * device and our operations, and display a welcome message */
+static int __init vga_led_init(void) {
+
+  printk(KERN_INFO "vga_led: init\n");
+
+  if ( (registers = ioremap(HW_REGS_BASE, HW_REGS_SIZE)) == NULL ) {
+    printk(KERN_ERR "vga_led: Mapping hardware registers failed\n");
+    return -1;
+  }
+
+  screen_registers = registers +
+    ((unsigned long) VGA_LED_0_BASE & (unsigned long) HW_REGS_MASK);
+
+  if (alloc_chrdev_region(&firstdev, 0, 1, "vgaled") < 0) goto unmap;
+  if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL) goto unregister;
+  if (device_create(cl, NULL, firstdev, NULL, "vgaled") == NULL) goto del_class;
+  cdev_init(&c_dev, &my_fops);
+  if (cdev_add(&c_dev, firstdev, 1) == -1) goto del_device;
+
+  /* Display a welcome message */
+ //write_center(320,240);
+  return 0;
+
+  /* Clean up if something went wrong */
+ unmap:      iounmap(registers);
+ del_device: device_destroy(cl, firstdev);
+ del_class:  class_destroy(cl);
+ unregister: unregister_chrdev_region(firstdev, 1);
+  return -1;
+}
+
+/* Disable the driver; undo the effects of the initialization routine */
+static void __exit vga_led_exit(void) {
+  printk(KERN_INFO "vga_led: exit\n");
+
+  cdev_del(&c_dev);
+  device_destroy(cl, firstdev);
+  class_destroy(cl);
+  unregister_chrdev_region(firstdev, 1);
+  iounmap(registers);
+}
+
+module_init(vga_led_init);
+module_exit(vga_led_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stephen A. Edwards, Columbia University");
+MODULE_DESCRIPTION("VGA 7-segment LED Emulator");
---
+++ vga_led.h
@@ -0,0 +1,41 @@
+/*
+ *
+ * Lab 3
+ *
+ * Name: Bernardo de Almeida Abreu
+ * UNI: bd2440
+ *
+ * Name: Tomas Mantelato
+ * UNI: tm2779
+ * 
+ */
+
+#ifndef _VGA_LED_H
+#define _VGA_LED_H
+
+#include <linux/ioctl.h>
+
+
+typedef struct {
+	short sprite_x, sprite_y;
+  	unsigned char shape;
+  	unsigned char count;
+  	unsigned char layer;	
+}sprite_info;
+
+typedef struct {
+	unsigned char life_1, life_2;
+  	unsigned int background_color;	
+}screen;
+
+typedef struct {
+	sprite_info sprite;
+	screen background;
+} vga_screen_arg_t;
+
+
+/* ioctls and their arguments */
+#define VGA_LED_WRITE_DIGIT _IOW('q', 1, vga_screen_arg_t *)
+#define VGA_LED_READ_DIGIT  _IOR('q', 2, vga_screen_arg_t *)
+
+#endif
